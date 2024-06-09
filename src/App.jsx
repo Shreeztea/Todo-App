3190b919c778c73660f81c18fa6f3389b2cdd1e7
ShristiOlive <katestha@hotmail.com>
2024-06-09 08:33:55 +0545

    Added Layout in single file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      this is my tset
+      <center className='todo-container'>
+          <h1>TODO APP</h1>
+          <div className="container text-center">
+            <div className="row">
+              <div className="col-6">
+                <input type="text" name="" id="" placeholder='Enter Todo here' />
+              </div>
+              <div className="col-4">
+                <input type="date" name="" id="" />
+              </div>
+              <div className="col-2">
+                <button type="button" className="btn btn-success">
+                  Add
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                Buy Milk
+              </div>
+              <div className="col-4">
+                4/10/2023
+              </div>
+              <div className="col-2">
+                <button type="button" className="btn btn-danger">
+                  Delete
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                Go to College
+              </div>
+              <div className="col-4">
+                4/10/2023
+              </div>
+              <div className="col-2">
+                <button type="button" className="btn btn-danger">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+      </center>
     </>
   )
 }
