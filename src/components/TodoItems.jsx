5d4bd71fb8727844de8fd284a1a3cafe33440fa7
ShristiOlive <katestha@hotmail.com>
2024-06-09 11:07:22 +0545

    Implemented css module

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,7 +1,8 @@
 import TodoItem from "./TodoItem";
+import styles from "./TodoItems.module.css";
 function TodoItems ({todoItems}) {
     return (
-        <div className="container items-container">
+        <div className={styles.itemsContainer}>
             {todoItems.map((item) => (
                 <TodoItem key={item.name} todoDate={item.date} todoName={item.name} />
             ))}
