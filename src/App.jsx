e7df9c3176063448d83543bd2b0663e82540b65a
Shreeztea <katestha@hotmail.com>
2024-07-23 23:06:06 +0545

    Implemented useState hook to add multiple items

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,31 @@ import AppName from './components/AppName'
 import AddTodo from './components/AddTodo'
 import TodoItems from './components/TodoItems'
 import './App.css'
+import { useState } from 'react'
 function App() {
-  const todoItems = [
-    {
-      name: "Buy Milk",
-      date: "4/10/2024"
-    },
-    {
-      name: "Go to school",
-      date: "5/10/2024"
-    },
-    {
-      name: "Do assingment",
-      date: "6/10/2026"
-    }
-  ]
+  const [todoItems,setTodoItems] = useState([])
+  const onNewItem = (todoName,dueDate) => {
+    setTodoItems([...todoItems,{name : todoName, date : dueDate}])
+  }
+  // const todoItems = [
+  //   {
+  //     name: "Buy Milk",
+  //     date: "4/10/2024"
+  //   },
+  //   {
+  //     name: "Go to school",
+  //     date: "5/10/2024"
+  //   },
+  //   {
+  //     name: "Do assingment",
+  //     date: "6/10/2026"
+  //   }
+  // ]
   return (
     <>
       <center className='todo-container'>
           <AppName />
-          <AddTodo />
+          <AddTodo onNewItem={onNewItem} />
           <TodoItems todoItems={todoItems}/>
       </center>
     </>
